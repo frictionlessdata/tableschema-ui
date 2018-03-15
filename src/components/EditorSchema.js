5a67c0e2eb5b7316457e3062c45bc840e5677339
roll <eskarev@gmail.com>
2018-03-15 12:50:24 +0300

    Added descriptor feedback

--- src/components/EditorSchema.js
+++ src/components/EditorSchema.js
@@ -4,17 +4,27 @@ const React = require('react')
 // Component
 
 const EditorSchema = ({descriptor, onSave}) => {
-  console.log(descriptor)
   const refs = {}
   return (
     <div className="container" style={{border: 'solid 1px #f00', padding: '1em'}}>
       <h3>Schema Editor</h3>
+
+      {/* Text */}
       <div className="form-group">
-        <textarea className="form-control" ref={(ref) => {refs.text = ref}} />
+        <textarea
+          className="form-control"
+          ref={(ref) => {refs.text = ref}}
+          defaultValue={descriptor}
+        />
       </div>
-      <a href="#" className="btn btn-primary" onClick={(ev) => onSave(refs.text.value)}>
-        Save
-      </a>
+
+      {/* Button */}
+      <div className="form-group">
+        <a href="#" className="btn btn-primary" onClick={(ev) => onSave(refs.text.value)}>
+          Save
+        </a>
+      </div>
+
     </div>
   )
 }
