e3a68fd849e5679afdbe5d05f6af4418f9aad00e
roll <eskarev@gmail.com>
2018-03-26 09:57:11 +0300

    Removed not used helper

--- src/helpers.js
+++ src/helpers.js
@@ -1,15 +1,4 @@
-const lodashDebounce = require('lodash/debounce')
-
-
-// Module API
-const debounce = (wait, func) => {
-  return lodashDebounce(func, wait)
-}
-
-
-
 // System
 
 module.exports = {
-  debounce,
 }
